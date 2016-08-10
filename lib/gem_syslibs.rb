f1f1b066cd86b746315f301ef41c3dba2f4e9b3d
Ahmed Borham <saladin2002eg@gmail.com>
2016-08-10 20:33:29 +0200

    change from captrue one gem to array of gems

--- lib/gem_syslibs.rb
+++ lib/gem_syslibs.rb
@@ -4,17 +4,15 @@ module GemSyslibs
   class Syslibs
 
     def check
-      begin
       definition = Bundler.definition
       definition.validate_ruby!
       deps = definition.dependencies
-      definition.resolve.materialize(deps,[])
-      rescue Bundler::GemNotFound, Bundler::VersionConflict, StandardError => e
-        # search the error message for gem name "Could not find gem '{gem_name} (~> version)' in any of the gem sources listed in your Gemfile or available on this machine."
-        missing_gem = e.message.scan(/(?<=')(.*)(?=')/)
-
-      end
-      #puts definition.dependencies
+      missing = []
+       deps.map(&:name).each do |g|
+        exists = %x(gem list -i #{g})
+         missing << g if exists.chop == "false"
+       end
+      puts missing.inspect
 
     end
 
